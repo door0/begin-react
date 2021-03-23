7dc991accecd4e2e4eb2230fe71b0bc7cbd3b1f4
dooooor0 <68139125+door0@users.noreply.github.com>
2021-03-23 14:12:10 +0900

    2021-03-23-event

--- src/App.js
+++ src/App.js
@@ -9,21 +9,63 @@ class App extends Component {
     constructor(props) { //컴포넌트 실행될때 render함수보다 먼저 실행되면서 컴포넌트를 초기화시켜주는 함수는 constructor.
         super(props);
         this.state = {
+            mode:'read', //이부분을 read로 바꾸면 render()도 바뀜
+            selected_content_id:0,
             subject:{title:'WEB', sub:'world wide web!'},
+            welcome:{title:'welcome', desc:'Hello, React~!'}, 
             contents:[
-                { id:1, title:'HTML', desc:'HTML is HyperText Markup Launguage.'},
+                { id:1, title:'HTML', desc:'HTML is for information.'},
                 { id:2, title:'CSS', desc:'CSS is for design.'},
                 { id:3, title:'JavaScript', desc:'JavaScript is for interactive.'}
             ]
         }
     }
     render() { 
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode === 'read') {
+            var i = 0;
+            while(i < this.state.contents.length) {
+                var data = this.state.contents[i];
+                if ( data.id === this.state.selected_content_id ) {
+                    _title = data.title;
+                    _desc = data.desc;
+                    break;
+                } 
+                i = i + 1;
+            }
+        }
         return ( //속성=props, subject(하위컨포넌트) props의 값(title, sub)들이 하드코딩되어있어서 state로 만들어야함. 
             <div className="App">
-            <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> 
-            <Subject title="React" sub="호로롤ㄹㄹ!"></Subject> {/**주석작성 */}
-            <TOC data={this.state.contents}></TOC>
-            <Content title="HTML" desc="HTML is HyperText Markup Launguage."></Content>
+            <Subject title={this.state.subject.title} sub={this.state.subject.sub}
+                onChangePage={function() { //내가 만든 함수,onChangePage
+                    this.setState({mode:'welcome'});
+                }.bind(this)}
+            ></Subject> 
+            {/* subject의 과도기적 코드
+            <header>
+                <h1><a href="/" onClick={function(e) {
+                    console.log(e);
+                    e.preventDefault();
+                    //this.state.mode = 'welcome';
+                    this.setState({
+                        mode:'welcome'
+                    });
+                }.bind(this)}>{this.state.subject.title}</a></h1> 
+                {this.state.subject.sub} 
+            </header>
+            */}
+            <TOC onChangePage={function(id) {
+                    this.setState({
+                        mode:'read',
+                        selected_content_id:Number(id)
+                    })
+                }.bind(this)} 
+                data={this.state.contents}
+            ></TOC>
+            <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,7 +4,10 @@ class Subject extends Component {
     render() { 
         return (
             <header> {/* 리팩토링 했다.*/}
-                <h1>{this.props.title}</h1> 
+                <h1><a href="/" onClick={function(e) {
+                    e.preventDefault();
+                    this.props.onChangePage();
+                }.bind(this)}>{this.props.title}</a></h1> 
                 {this.props.sub}
             </header>
         );
