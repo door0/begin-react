dcdb7a4ab7acad6e7df0cbc0b7c21dc029f571b0
dooooor0 <68139125+door0@users.noreply.github.com>
2021-03-23 10:06:54 +0900

    2021-03-23-state

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import { Component } from 'react';
 
 //컴포넌트를 만드는 것 -template (현재 4개의 컴포넌트..)
 class App extends Component {
+    constructor(props) { //컴포넌트 실행될때 render함수보다 먼저 실행되면서 컴포넌트를 초기화시켜주는 함수는 constructor.
+        super(props);
+        this.state = {
+            subject:{title:'WEB', sub:'world wide web!'},
+            contents:[
+                { id:1, title:'HTML', desc:'HTML is HyperText Markup Launguage.'},
+                { id:2, title:'CSS', desc:'CSS is for design.'},
+                { id:3, title:'JavaScript', desc:'JavaScript is for interactive.'}
+            ]
+        }
+    }
     render() { 
-        return (
+        return ( //속성=props, subject(하위컨포넌트) props의 값(title, sub)들이 하드코딩되어있어서 state로 만들어야함. 
             <div className="App">
-            <Subject title="web" sub="world wide web!"></Subject> {/* 속성을 props라고 함*/}
-            <Subject title="React" sub="호로롤ㄹㄹ!"></Subject> 
-            <TOC></TOC>
+            <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> 
+            <Subject title="React" sub="호로롤ㄹㄹ!"></Subject> {/**주석작성 */}
+            <TOC data={this.state.contents}></TOC>
             <Content title="HTML" desc="HTML is HyperText Markup Launguage."></Content>
             </div>
         );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,13 +1,18 @@
 import { Component } from 'react'; 
 
 class TOC extends Component {
-    render() { 
+    render() { //복수의 엘리먼트를 생성할때는 key라는 특수한 props를 사용해야함.
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while( i < data.length ) {
+            lists.push(<li key={data[i].id}><a href={"/contents/"+data[i].id}>{data[i].title}</a></li>)
+            i= i + 1;
+        }
         return (
             <nav>
                 <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
+                    {lists}
                 </ul>
             </nav>
         );
